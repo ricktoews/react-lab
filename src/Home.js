c2ddaf6d2752368f233e0d72ef3302e1dc35e206
Rick Toews <rick.toews@gmail.com>
2018-04-09 00:13:24 -0700

    Add material-ui@next to project.

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="container">
+    <h1>ricktoews.me Home</h1>
+    <p>Photos, teasers, anything else that might look good on a home page should go here.</p>
+  </div>
+)
+
+export default Home;
